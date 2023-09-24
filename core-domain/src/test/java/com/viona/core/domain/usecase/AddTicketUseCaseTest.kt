b099b2e1bd96cca9387cc2efb169fe75dcce842f
viona <vionamahdiya@gmail.com>
2023-09-24 20:33:21 +0700

    Setup 9.1: Add unit test on use case add ticket

--- core-domain/src/test/java/com/viona/core/domain/usecase/AddTicketUseCaseTest.kt
+++ core-domain/src/test/java/com/viona/core/domain/usecase/AddTicketUseCaseTest.kt
@@ -2,6 +2,7 @@ package com.viona.core.domain.usecase
 
 import com.viona.core.domain.model.Ticket
 import com.viona.core.domain.util.FakeTicketRepository
+import com.viona.core.domain.util.InvalidException
 import io.mockk.clearAllMocks
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
@@ -32,6 +33,42 @@ class AddTicketUseCaseTest {
 
         // Then
         assertEquals(Unit, result)
+    }
+
+    @Test
+    fun `Add ticket use case driver name empty, should return exception`() = runBlocking {
+        // Given
+        val ticket = Ticket(
+            driverName = "", timestamp = "12", licenseNumber = 123,
+            inboundWeight = 23, outboundWeight = 10, netWeight = 13, id = 1
+        )
+        // When
+        val result = addTicket(ticket)
+
+        // Then
+        assertEquals(InvalidException("The Driver Name cant be empty"), result)
+    }
+
+    @Test(expected = InvalidException::class)
+    fun `should throw InvalidException when driver name is empty`() = runBlocking {
+        // Given
+        val ticket = Ticket(
+            driverName = "", timestamp = "12", licenseNumber = 123,
+            inboundWeight = 23, outboundWeight = 10, netWeight = 13, id = 1
+        )
+        // When
+        addTicket(ticket)
+    }
+
+    @Test(expected = InvalidException::class)
+    fun `should throw InvalidException when license number is 0`() = runBlocking {
+        // Given
+        val ticket = Ticket(
+            driverName = "vio", timestamp = "12", licenseNumber = 0,
+            inboundWeight = 23, outboundWeight = 10, netWeight = 13, id = 1
+        )
+        // When
+        addTicket(ticket)
     }
 
     @After
